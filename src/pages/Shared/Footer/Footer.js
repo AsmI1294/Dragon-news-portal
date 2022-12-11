229bdcc1491248f09b35916b1238d788cb82d80a
ASMIttehad <ittehad.akash@gmail.com>
2022-12-11 20:46:01 +0100

    footer changed

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div> Footer</div>;
+  return (
+    <div className="text-center bg-secondary" style={{ height: "10vh" }}>
+      <p className="text-white pt-4">Dragon News Footer</p>
+    </div>
+  );
 };
 
 export default Footer;
